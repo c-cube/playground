dd7c9661f26cb398f1662a0e148e2d7b0817ab00
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-06-03 14:50:43 -0400

    compare with records

--- bench_objects/README.md
+++ bench_objects/README.md
@@ -6,6 +6,7 @@ this just tests the cost of a few method calls via a virtual class.
 output:
 
 ```
-N=100000000
-100000000 iterations in 1.179s (84850904.203/s; 12ns / iteration)
+N=1000000000
+OBJ: 1000000000 iterations in 8.575s (116617758.833/s; 9ns / iteration)
+REC: 1000000000 iterations in 4.776s (209386907.707/s; 4.8ns / iteration)
 ```
--- bench_objects/bench.ml
+++ bench_objects/bench.ml
@@ -1,39 +1,82 @@
+module Rec = struct
 
+  type reader = {
+    get_i: unit -> int;
+    get_end : unit -> unit
+  }
 
-class virtual reader = object
-  method virtual get_i : int
-  method virtual get_end : unit
+  let[@inline] foo (f:reader) =
+    let x = f.get_i () in
+    let y = f.get_i () in
+    f.get_end();
+    x+y
+
+  let mk_reader () =
+    let x = ref 0 in
+    { get_i= (fun () ->x := !x+1; !x);
+      get_end = ignore
+    }
+end
+
+module O = struct
+
+  class virtual reader = object
+    method virtual get_i : int
+    method virtual get_end : unit
   end
 
 
-let[@inline] foo (f:reader) =
-  let x = f#get_i in
-  let y = f#get_i in
-  f#get_end;
-  x+y
+  let[@inline] foo (f:reader) =
+    let x = f#get_i in
+    let y = f#get_i in
+    f#get_end;
+    x+y
 
-let mk_reader () = object
-  inherit reader
-  val mutable x = 0
-  method get_i = x <- x+1; x
-  method get_end = ()
+  let mk_reader () = object
+    inherit reader
+    val mutable x = 0
+    method get_i = x <- x+1; x
+    method get_end = ()
   end
+end
 
 let now_s () = (Mtime.to_uint64_ns (Mtime_clock.now ()) |> Int64.to_float) *. 1e-9
 
 let () =
-  let n = try Sys.getenv "N" |> int_of_string with _ -> 100_000_000 in
+  let n = try Sys.getenv "N" |> int_of_string with _ -> 1_000_000_000 in
   Printf.printf "N=%d\n%!" n;
-  let t_start = now_s() in
-  Sys.opaque_identity (
-    let r = mk_reader() in
-    for i=1 to n do
-      let _x = foo r in
-      ignore @@ Sys.opaque_identity _x
-    done;
-  );
-  let elapsed_s = now_s () -. t_start in
-  Printf.printf "%d iterations in %.3fs (%.3f/s; %.fns / iteration)\n%!"
-    n elapsed_s
-    (float n /. elapsed_s) (elapsed_s /. float n *. 1e9);
+
+  begin
+    let open O in
+    let t_start = now_s() in
+    Sys.opaque_identity (
+      let r = mk_reader() in
+      for i=1 to n do
+        let _x = foo r in
+        ignore @@ Sys.opaque_identity _x
+      done;
+    );
+    let elapsed_s = now_s () -. t_start in
+    Printf.printf "OBJ: %d iterations in %.3fs (%.3f/s; %.fns / iteration)\n%!"
+      n elapsed_s
+      (float n /. elapsed_s) (elapsed_s /. float n *. 1e9);
+  end;
+
+  begin
+    let open Rec in
+    let t_start = now_s() in
+    Sys.opaque_identity (
+      let r = mk_reader() in
+      for i=1 to n do
+        let _x = foo r in
+        ignore @@ Sys.opaque_identity _x
+      done;
+    );
+    let elapsed_s = now_s () -. t_start in
+    Printf.printf "REC: %d iterations in %.3fs (%.3f/s; %.1fns / iteration)\n%!"
+      n elapsed_s
+      (float n /. elapsed_s) (elapsed_s *. 1e9 /. float n);
+  end;
+
   ()
+
