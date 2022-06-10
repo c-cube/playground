2468869bacf606ab4d91b6855de0e0f5816c4550
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-06-10 15:29:37 -0400

    bench object: also measure 1st-class modules and direct call

--- bench_objects/bench.ml
+++ bench_objects/bench.ml
@@ -18,6 +18,30 @@ module Rec = struct
     }
 end
 
+module Mod = struct
+  module type READER = sig
+    val get_i : unit-> int
+    val get_end : unit -> unit
+  end
+
+  type reader = (module READER)
+
+  let[@inline] foo (f:reader) =
+    let (module R) = f in
+    let x = R.get_i () in
+    let y = R.get_i () in
+    R.get_end();
+    x+y
+
+  let mk_reader () : reader =
+    let module R = struct
+      let x = ref 0
+      let get_i () = x := !x+1; !x
+      let get_end = ignore
+    end in
+    (module R)
+end
+
 module O = struct
 
   class virtual reader = object
@@ -57,9 +81,25 @@ let () =
       done;
     );
     let elapsed_s = now_s () -. t_start in
-    Printf.printf "OBJ: %d iterations in %.3fs (%.3f/s; %.fns / iteration)\n%!"
+    Printf.printf "OBJ: %d iterations in %.3fs (%.3f/s; %.1fns / iteration)\n%!"
       n elapsed_s
-      (float n /. elapsed_s) (elapsed_s /. float n *. 1e9);
+      (float n /. elapsed_s) (elapsed_s *. 1e9 /. float n);
+  end;
+
+  begin
+    let open Mod in
+    let t_start = now_s() in
+    Sys.opaque_identity (
+      let r = mk_reader() in
+      for i=1 to n do
+        let _x = foo r in
+        ignore @@ Sys.opaque_identity _x
+      done;
+    );
+    let elapsed_s = now_s () -. t_start in
+    Printf.printf "MOD: %d iterations in %.3fs (%.3f/s; %.1fns / iteration)\n%!"
+      n elapsed_s
+      (float n /. elapsed_s) (elapsed_s *. 1e9 /. float n);
   end;
 
   begin
@@ -74,6 +114,32 @@ let () =
     );
     let elapsed_s = now_s () -. t_start in
     Printf.printf "REC: %d iterations in %.3fs (%.3f/s; %.1fns / iteration)\n%!"
+      n elapsed_s
+      (float n /. elapsed_s) (elapsed_s *. 1e9 /. float n);
+  end;
+
+  begin
+    (* no indirection *)
+    let x = ref 0 in
+    let get_i= fun () ->x := !x+1; !x in
+    let get_end = ignore in
+
+    let[@inline] foo () =
+      let x = get_i () in
+      let y = get_i () in
+      get_end();
+      x+y
+    in
+
+    let t_start = now_s() in
+    Sys.opaque_identity (
+      for i=1 to n do
+        let _x = foo () in
+        ignore @@ Sys.opaque_identity _x
+      done;
+    );
+    let elapsed_s = now_s () -. t_start in
+    Printf.printf "INLINE CODE: %d iterations in %.3fs (%.3f/s; %.1fns / iteration)\n%!"
       n elapsed_s
       (float n /. elapsed_s) (elapsed_s *. 1e9 /. float n);
   end;
