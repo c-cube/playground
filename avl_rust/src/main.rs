8c9a3d3bae7f7aaf7b9ce339d2b644e336b8dbf1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-01-10 15:50:22 -0500

    use mimalloc; count number of rebalance ops

--- avl_rust/src/main.rs
+++ avl_rust/src/main.rs
@@ -1,9 +1,16 @@
+
 use avl_test::*;
+
+#[global_allocator]
+static ALLOCATOR: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 fn main() {
     let t = Tree::range(5_000_000);
+    //let t = Tree::range(500);
+    //println!("tree: {}", t.print());
     println!("depth: {}", t.depth());
     let v = t.traverse();
     println!("v.len: {:?}", v.len());
     dbg!(sorted(&v));
+    dbg!(avl_test::n_rebalance());
 }
