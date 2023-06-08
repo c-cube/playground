2832c0aadefb3d730bd7543615c475abbe316381
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-06-07 21:55:46 -0400

    add try_lock to the bench_uncontended_mutex

--- bench_uncontended_mutex/bin/main.ml
+++ bench_uncontended_mutex/bin/main.ml
@@ -1,11 +1,25 @@
-let () =
-  let n = 40_000_000 in
+let run n =
   let lock = Mutex.create() in
   for _i=1 to n do
     Mutex.lock lock;
     Sys.opaque_identity ();
     Mutex.unlock lock;
-  done;
+  done
 
-  Printf.printf "done (%d iterations)\n%!" n;
-  ()
+let run_try n =
+  let lock = Mutex.create() in
+  for _i=1 to n do
+    let ok =Mutex.try_lock lock in
+    assert ok;
+    Sys.opaque_identity ();
+    Mutex.unlock lock;
+  done
+
+let () =
+  let n = 40_000_000 in
+  (match Sys.argv.(1) with
+  | exception _ -> run n
+  | "try" -> run_try n
+  | _ -> failwith {|expected nothing or "try"|}
+  );
+  Printf.printf "done (%d iterations)\n%!" n
--- bench_uncontended_mutex/run.sh
+++ bench_uncontended_mutex/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 dune build --profile=release bin/main.exe
 ./_build/default/bin/main.exe
-hyperfine './_build/default/bin/main.exe'
+hyperfine './_build/default/bin/main.exe' './_build/default/bin/main.exe try'
 
 echo 'run: `qalc "610ms / 40M"`'
 qalc '610ms / 40M'
