4498f11ecbd46de3dc5d037015bb8677478f226a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-01-01 21:30:48 -0500

    tiny-rg.rs: parallel version

--- rust/tiny-rg/src/main.rs
+++ rust/tiny-rg/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     io::BufRead,
     path::PathBuf,
     sync::{
@@ -10,14 +11,13 @@ use std::{
 
 use anyhow::{Context, Result};
 use clap::Parser;
+use rayon::prelude::*;
 use regex::Regex;
 
 #[derive(Debug, Parser)]
 struct Cli {
     regex: Regex,
     dirs: Vec<String>,
-    // level of parallelism
-    // j: Option<u8>
 }
 
 #[derive(Debug, Default)]
@@ -25,7 +25,15 @@ struct Stats {
     files: AtomicU64,
     lines: AtomicU64,
     matches: AtomicU64,
-    errors: Mutex<Vec<anyhow::Error>>,
+    errors: Mutex<HashSet<String>>,
+}
+
+impl Stats {
+    fn add_err(&self, err: impl Into<anyhow::Error>) {
+        let err = format!("{}", err.into().root_cause());
+        let mut errors = self.errors.lock().unwrap();
+        errors.insert(err);
+    }
 }
 
 fn process_file(regex: &Regex, path: PathBuf, stats: Arc<Stats>) -> Result<()> {
@@ -35,6 +43,9 @@ fn process_file(regex: &Regex, path: PathBuf, stats: Arc<Stats>) -> Result<()> {
     let mut file = std::io::BufReader::new(std::fs::File::open(&path)?);
 
     let mut line_buf = String::with_capacity(64 * 1024);
+    let mut line_count = 0;
+    let mut match_count = 0;
+
     loop {
         line_buf.clear();
         let n = file.read_line(&mut line_buf)?;
@@ -42,36 +53,34 @@ fn process_file(regex: &Regex, path: PathBuf, stats: Arc<Stats>) -> Result<()> {
             break;
         }
 
-        stats.lines.fetch_add(1, Ordering::SeqCst);
+        line_count += 1;
 
         let line = &line_buf[0..n - 1];
         if regex.is_match(&line) {
-            stats.matches.fetch_add(1, Ordering::Relaxed);
+            match_count += 1;
 
-            let basename = path.file_name().unwrap_or(path.as_os_str()).display();
-            println!("{basename}: {line:?}");
+            let _basename = path.file_name().unwrap_or(path.as_os_str()).display();
+            // println!("{_basename}: {line:?}");
         }
     }
 
+    stats.lines.fetch_add(line_count, Ordering::SeqCst);
+    stats.matches.fetch_add(match_count, Ordering::Relaxed);
     Ok(())
 }
 
-fn process_dir(regex: &Regex, dir: String, stats: Arc<Stats>) -> Result<()> {
-    for file in walkdir::WalkDir::new(dir) {
-        let file = match file {
-            Ok(f) if f.file_type().is_file() => f,
-            Ok(_) => continue,
+/// Iterate on relevant files
+fn dir_entries(dir: String, stats: Arc<Stats>) -> impl Iterator<Item = PathBuf> {
+    walkdir::WalkDir::new(dir).into_iter().filter_map(
+        move |file: walkdir::Result<walkdir::DirEntry>| match file {
+            Ok(f) if f.file_type().is_file() => Some(f.into_path()),
+            Ok(_) => None,
             Err(err) => {
-                let mut errors = stats.errors.lock().unwrap();
-                errors.push(err.into());
-                continue;
+                stats.add_err(err);
+                None
             }
-        };
-
-        process_file(regex, file.into_path(), stats.clone())?;
-    }
-
-    Ok(())
+        },
+    )
 }
 
 fn main() -> Result<()> {
@@ -83,8 +92,20 @@ fn main() -> Result<()> {
 
     let stats = Arc::new(Stats::default());
 
-    for dir in &cli.dirs {
-        process_dir(&cli.regex, dir.clone(), stats.clone())?;
+    {
+        let stats1 = stats.clone();
+        let stats2 = stats.clone();
+        cli.dirs
+            .iter()
+            .cloned()
+            .flat_map(move |dir| dir_entries(dir, stats1.clone()))
+            // .par_bridge()
+            .for_each(
+                move |path| match process_file(&cli.regex, path, stats2.clone()) {
+                    Ok(()) => (),
+                    Err(err) => stats2.add_err(err),
+                },
+            )
     }
 
     let t_stop = Instant::now();
